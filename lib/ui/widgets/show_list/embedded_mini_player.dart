08cc7651287101550b0bf0d5374a7f5af6631397
jamart3d <jamart3d@gmail.com>
2026-03-06 16:34:08 -0800

    Fix: Applied correct single-quote standardization to embedded_mini_player.dart _formatDuration.

--- lib/ui/widgets/show_list/embedded_mini_player.dart
+++ lib/ui/widgets/show_list/embedded_mini_player.dart
@@ -16,13 +16,13 @@ class EmbeddedMiniPlayer extends StatelessWidget {
 
   String _formatDuration(Duration duration) {
     if (duration == Duration.zero) return '0:00';
-    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
     String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
     String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
     if (duration.inHours > 0) {
-      return "${duration.inHours}:$twoDigitMinutes:$twoDigitSeconds";
+      return '${duration.inHours}:$twoDigitMinutes:$twoDigitSeconds';
     }
-    return "${duration.inMinutes}:$twoDigitSeconds";
+    return '${duration.inMinutes}:$twoDigitSeconds';
   }
 
   @override
